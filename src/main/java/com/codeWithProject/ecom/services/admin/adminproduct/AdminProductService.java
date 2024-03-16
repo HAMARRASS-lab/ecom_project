c22b22ec6d1db4d7512cc9172496952ed9fbbe84
hamza.hamarras <hamza.hamarras@inetum.com>
2024-03-16 13:22:40 +0000

    add product, and get product from db

--- src/main/java/com/codeWithProject/ecom/services/admin/adminproduct/AdminProductService.java
+++ src/main/java/com/codeWithProject/ecom/services/admin/adminproduct/AdminProductService.java
@@ -1,4 +1,10 @@
 package com.codeWithProject.ecom.services.admin.adminproduct;
 
+import com.codeWithProject.ecom.dto.ProductDto;
+
+import java.util.List;
+
 public interface AdminProductService {
+    public ProductDto addProduct(ProductDto productDto);
+    public List<ProductDto> getAllProducts();
 }
--- src/main/java/com/codeWithProject/ecom/services/admin/adminproduct/AdminProductServiceImp.java
+++ src/main/java/com/codeWithProject/ecom/services/admin/adminproduct/AdminProductServiceImp.java
@@ -1,10 +1,39 @@
 package com.codeWithProject.ecom.services.admin.adminproduct;
 
+import com.codeWithProject.ecom.dto.ProductDto;
+import com.codeWithProject.ecom.entity.Category;
+import com.codeWithProject.ecom.entity.Product;
+import com.codeWithProject.ecom.repository.CategoryRepository;
+import com.codeWithProject.ecom.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import  java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
-public class AdminProductServiceImp implements AdminProductService{
+@RequiredArgsConstructor
+public class AdminProductServiceImp implements AdminProductService {
 
+    private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
 
+    public ProductDto addProduct(ProductDto productDto){
+        Product product=new Product();
+        product.setName(product.getName());
+        product.setDescription(product.getDescription());
+        product.setPrice(product.getPrice());
+        byte[] imgBytes = product.getImg();
+        product.setImg(imgBytes);
+          Category category=categoryRepository.findById(productDto.getCategoryId()).orElseThrow();
 
+          product.setCategory((category));
+          return productRepository.save(product).getDto();
+    }
+
+    public List<ProductDto> getAllProducts(){
+        List<Product> products=productRepository.findAll();
+        return  products.stream().map(Product::getDto).collect(Collectors.toList());
+    }
 }
+
