a1c19fcf7a9481d60ed38df82fa62b671efab822
hamza.hamarras <hamza.hamarras@inetum.com>
2024-05-21 22:27:47 +0100

    add order status

---
+++ src/main/java/com/codeWithProject/ecom/repository/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.codeWithProject.ecom.repository;
+
+import com.codeWithProject.ecom.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+
+}
