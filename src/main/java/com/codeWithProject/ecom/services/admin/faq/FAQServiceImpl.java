c8005677a7affd50c418ef2f6d999d12536aeefc
hamza.hamarras <hamza.hamarras@inetum.com>
2024-08-29 16:09:14 +0100

    all orders backend

---
+++ src/main/java/com/codeWithProject/ecom/services/admin/faq/FAQServiceImpl.java
@@ -0,0 +1,10 @@
+package com.codeWithProject.ecom.services.admin.faq;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FAQServiceImpl implements  FAQService{
+}
