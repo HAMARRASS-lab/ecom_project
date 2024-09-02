3f110505599d3c7cc59551d44911d117af81eb0e
hamza.hamarras <hamza.hamarras@inetum.com>
2024-09-02 13:46:56 +0100

    add fag back

--- src/main/java/com/codeWithProject/ecom/services/admin/faq/FAQServiceImpl.java
+++ src/main/java/com/codeWithProject/ecom/services/admin/faq/FAQServiceImpl.java
@@ -1,10 +1,39 @@
 package com.codeWithProject.ecom.services.admin.faq;
 
 
+import com.codeWithProject.ecom.dto.FAQDto;
+import com.codeWithProject.ecom.entity.FAQ;
+import com.codeWithProject.ecom.entity.Product;
+import com.codeWithProject.ecom.repository.FAQRepository;
+import com.codeWithProject.ecom.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class FAQServiceImpl implements  FAQService{
+
+
+    private  final FAQRepository faqRepository;
+    private final ProductRepository productRepository;
+
+    public FAQDto postFAQ( Long productId, FAQDto faqDto){
+        Optional<Product> optionalProduct=productRepository.findById(productId);
+
+        if(optionalProduct.isPresent()){
+           FAQ faq = new FAQ();
+
+           faq.setAnswer(faqDto.getQuestion());
+           faq.setAnswer(faqDto.getAnswer());
+           faq.setProduct(optionalProduct.get());
+
+           return faqRepository.save(faq).getFAQDto();
+
+        }
+        return  null;
+    }
+
+
 }
